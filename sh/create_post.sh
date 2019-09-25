dcd9082f2a8232cd8aab8a07f0fd2d5e08761fa8
Giovana Morais <giovana.vmorais@gmail.com>
2019-09-25 14:23:01 -0300

    adicionando script pra criação de template de post no jekyll

---
+++ sh/create_post.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# script pra criar header dos posts do Jekyll já com a data certa
+
+nome_arquivo=$1
+data_arquivo=$(date +"%Y-%m-%d")
+
+arquivo="${data_arquivo}-${nome_arquivo}.md"
+
+echo "Criando arquivo $arquivo"
+
+echo "---" >> $arquivo
+echo "layout: post" >> $arquivo
+echo "date: $(date +"%Y-%m-%d %H:%M")" >> $arquivo
+echo "image: " >> $arquivo
+echo "headerImage: false" >> $arquivo
+echo "tag: " >> $arquivo
+echo "category: blog" >> $arquivo
+echo "author: giovanamorais" >> $arquivo
+echo "description: " >> $arquivo
+echo "---" >> $arquivo
+
